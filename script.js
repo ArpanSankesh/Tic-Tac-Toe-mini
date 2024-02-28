478288e935ee6d6ef32844960e879c7c2c8ccf5b
ArpanSankesh <arpansankesh830@gmail.com>
2024-02-28 18:49:41 +0530

    udpates

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-let boxes = document.querySelectorAll('.box');
+let boxes = document.querySelectorAll(".box");
+let resetBt = document.querySelectorAll(".reset-btn");
+
+let turn = true;
+
+boxes.forEach((box) => {
+  box.addEventListener("click", function () {
+    if (turn){
+        box.innerHTML = "X"
+        turn = false;
+    }
+    else{
+        box.innerHTML = "O"
+        turn = true;
+    }
+    box.disa
+  });
+});
